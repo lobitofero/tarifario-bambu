a691868ff5e3d0cb899fbbfd7fa95fb1ff770705
lobitofero <154683983+lobitofero@users.noreply.github.com>
2026-06-24 22:12:46 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'tarifario-bambu-v1';
+const ARCHIVOS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARCHIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((respuestaCache) => {
+      if (respuestaCache) return respuestaCache;
+      return fetch(event.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
